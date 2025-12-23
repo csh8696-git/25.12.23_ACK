7ed6e5f18fb20f09084d01e6251336288afe0565
csh8696-git <csh8696@gmail.com>
2025-12-23 09:52:35 +0900

    ACK9
    짝수의 합

---
+++ solution9.cpp
@@ -0,0 +1,23 @@
+﻿#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+    int answer = 0;
+    for (int i = 0; i <= n; i++) {
+        if (i % 2 == 0) {
+            answer += i;
+        }
+    }
+    return answer;
+}
+//이하 괴상한 풀이법들
+
+//int solution(int n) {
+//    int answer = n / 2;
+//    answer *= answer + 1;
+//    return answer;
+//}
+
+//int solution(int n) { return (n >> 1) * ((n >> 1) + 1); }
