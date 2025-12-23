3422ebcc79ccbd20f0c8776b318691b2de1b3509
csh8696-git <csh8696@gmail.com>
2025-12-23 09:47:04 +0900

    ACK
    각도기

--- solution7.cpp
+++ solution7.cpp
@@ -24,3 +24,22 @@ int solution(int num1, int num2) {
 //    int answer = ((num1 * 1.0) / num2) * 1000;
 //    return answer;
 //}
+
+//int solution(int num1, int num2) {
+//    num1 *= 1000u;
+//    int q = 0;
+//    int shift = 0;
+//
+//    while ((num2 << shift) <= num1 && shift < 31) ++shift;
+//    if (shift) --shift;
+//
+//    for (; shift >= 0; --shift) {
+//        int cur = num2 << shift;
+//        q <<= 1;
+//        if (num1 >= cur) {
+//            num1 -= cur;
+//            q |= 1;
+//        }
+//    }
+//    return q;
+//}
---
+++ solution8.cpp
@@ -0,0 +1,41 @@
+﻿#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int angle) {
+    int answer = 0;
+    if (0 < angle && angle < 90) {
+        answer = 1;
+    }
+    else if (angle == 90) {
+        answer = 2;
+    }
+    else if (angle < 180) {
+        answer = 3;
+    }
+    else if (angle == 180) {
+        answer = 4;
+    }
+    return answer;
+} //이것 보다 단순화 가능할 것 같은데..
+
+//int solution(int angle) {
+//    return angle % 90 == 0 ? angle / 90 * 2 : (angle / 90) * 2 + 1;
+//}//쉣
+
+//int solution(int angle) {
+//    return angle > 90 ? (angle == 180 ? 4 : 3) : (angle == 90 ? 2 : 1);
+//}
+
+//int solution(int angle) {
+//    if (angle <= 90)
+//    {
+//        return angle < 90 ? 1 : 2;
+//    }
+//
+//    else
+//    {
+//        return angle < 180 ? 3 : 4;
+//    }
+//}
