e2b2235997b5521f20a0772f7fb23b9b2ea1ac19
csh8696-git <csh8696@gmail.com>
2025-12-23 09:24:35 +0900

    ACK5
    숫자 비교하기

---
+++ solution5.cpp
@@ -0,0 +1,21 @@
+﻿#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int num1, int num2) {
+    int answer = 0;
+    if (0 <= num1 && num1 <= 10000 && 0 <= num2 && num2 <= 10000) {
+        if (num1 == num2) {
+            answer = 1;
+        }
+        else {
+            answer = -1;
+        }
+    }
+    return answer;
+}
+
+//int solution(int num1, int num2) {
+//    return num1 == num2 ? 1 : -1;
+//}
