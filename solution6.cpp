8fb29d00d0de381ebc92dd901d223dcceea10203
csh8696-git <csh8696@gmail.com>
2025-12-23 09:37:05 +0900

    ACK7
    두 수의 합

---
+++ solution6.cpp
@@ -0,0 +1,26 @@
+﻿#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int num1, int num2) {
+    int answer = -1;
+    if (-50000 <= num1 && num1 <= 50000 && -50000 <= num2 && num2 <= 50000) {
+        answer = num1 + num2;
+    }
+    return answer;
+}
+
+
+//int Add(int x, int y) {
+//    while (y != 0) {
+//        unsigned carry = x & y;
+//        x = x ^ y;
+//        y = carry << 1;
+//    }
+//    return x;
+//}
+//
+//int solution(int num1, int num2) {
+//    return Add(num1, num2);
+//}
---
+++ solution7.cpp
@@ -0,0 +1,26 @@
+﻿#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int num1, int num2) {
+    int answer = 0;
+    double A = 0;
+    double B = 0;
+    if (0 < num1 && num1 <= 100 && 0 < num2 && num2 <= 100) {
+        A = num1;
+        B = num2;
+        answer = 1000 * (A / B);
+    }
+    return answer;
+}
+
+
+//int solution(int num1, int num2) {
+//    return (num1 * 1000) / num2;
+//}
+
+//int solution(int num1, int num2) {
+//    int answer = ((num1 * 1.0) / num2) * 1000;
+//    return answer;
+//}
