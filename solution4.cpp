f01f02fc807cab917780022b3d8f7d974b2d757e
csh8696-git <csh8696@gmail.com>
2025-12-23 09:18:57 +0900

    ACK4
    나이 출력

---
+++ solution4.cpp
@@ -0,0 +1,12 @@
+﻿#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int age) {
+    int answer = 0;
+    if (0 < age && age <= 120) {
+        answer = 2023 - age;
+    }
+    return answer;
+}
